427049546151a93100146fa8a1094e6b7d376a5d
Daniel Moreno <dmoreno12@unisalle.edu.co>
2026-04-03 13:07:06 -0500

    Se agrega los estados y tipos; ademas se agrega las interfaces de contratos

---
+++ IPaymentProcessor.java
@@ -0,0 +1,9 @@
+import java.util.Map;
+import java.util.List;
+
+public interface IInventoryService {
+    Product getProductInfo(String productCode);
+    boolean checkStock(String productCode, int quantity);
+    boolean updateStock(String productCode, int quantityDecrease);
+    List<Product> getAllProducts();
+}
